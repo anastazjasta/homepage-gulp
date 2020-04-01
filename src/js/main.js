fece8b2bcb36acdfa9503868d602d1b875d55851
Anastazjasta <anastazja.sta@gmail.com>
2020-04-01 21:20:39 +0200

    feat: new background

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,89 @@
-"use strict";
+ "use strict";
 
-// service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+// const hello = (name, age) => console.log(`Witaj ${name}, masz ${age} lat.`);
+
+// hello ("Anastazja", 25); 
+
+// const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+// const navigationList = document.querySelector('.navigation__list--js');
+
+// navigationSwitcher.addEventListener('click', (e) =>{
+// navigationList.classList.toggle('navigation__list--visible');
+// if (navigationList.classList.contains('navigation__list--visible')){
+//   navigationSwitcher.innerHTML = "x";
+// }s
+// else {
+//   navigationSwitcher.innerHTML ="≡";
+// }
+// });
+
+
+
+
+
+
+// const faces = ['😽', '❤', '🐱‍🚀'];
+
+// for (let i = 0; i < faces.length; i++){
+//   console.log(`element ${i+1}: ${faces[i]}`);
+// }
+
+
+// let a = 3;
+//  while (a<9) {
+//    a= a+3;
+//    console.log(a)
+
+//  }
+//  console.log(a);
+
+//  do {
+//   console.log(a);
+//  } while (a <9)
+
+ const person = {
+   name: 'anastazja',
+   age: 26
+ }
+
+//  for (let propertyName in person) {
+//    console.log(`${propertyName}: ${person[propertyName]}`);
+//  }
+
+//  console.log(document.cookie);
+//  document.cookie = "testoweCiastko = wartość";
+//  document.cookie = "testoweCiastko2 = wartość2";
+//  console.log(document.cookie);
+
+// console.log(person);
+
+// const jsonPerson = JSON.stringify(person);
+
+
+// console.log(jsonPerson);
+// localStorage.setItem('name','anastazja');
+//  localStorage.setItem('person',jsonPerson);
+
+
+// console.log(localStorage.getItem('name'));
+
+//   const personStringify = localStorage.getItem('person');
+
+
+
+//   const newPerson = JSON.parse(personStringify);
+
+//   console.log(newPerson);
+
+// localStorage.removeItem('person');
+
+const focusInput = document.querySelector('.focus--js');
+
+if (localStorage.getItem('focusInput')) {
+  focusInput.value = localStorage.getItem('focusInput');
 }
 
-// place your code below
-
-
-const hello = (name, age) => console.log(`Witaj ${name}, masz ${age} lat.`);
-
-hello ("Anastazja", 25); 
-
-const navigationSwitcher = document.querySelector('.navigation__switcher--js');
-const navigationList = document.querySelector('.navigation__list--js');
-
-navigationSwitcher.addEventListener('click', (e) =>{
-navigationList.classList.toggle('navigation__list--visible');
-if (navigationList.classList.contains('navigation__list--visible')){
-  navigationSwitcher.innerHTML = "x";
-}
-else {
-  navigationSwitcher.innerHTML ="≡";
-}
+focusInput.addEventListener ('keyup', (e) =>{
+  localStorage.setItem('focusInput', e.target.value);
 });
-
-
-
-
